f2b75a02924fbc20fa2321f38ee83b9086fd2efb
Sarah Thiery <sarahvthiery@gmail.com>
2021-02-28 18:40:15 -0500

    End of day

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,9 @@ class Game < ApplicationRecord
     belongs_to :user
     has_many :rounds 
     has_many :words, through: :rounds
+
+    def game_user
+        self.user.username
+    end
+
 end
--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :score
+  attributes :id, :user_id, :score, :game_user
 end
